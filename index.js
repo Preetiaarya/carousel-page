9a27fd4ccf86de2ffe1cc6d1ebb7fc693030a435
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2024-09-14 20:08:40 +0530

    some changes html, css and js

--- index.js
+++ index.js
@@ -3,36 +3,48 @@ let back = document.querySelector(".back");
 let next = document.querySelector(".next");
 
 let i = 0;
-let text = ["1", "2", "3", "4", "5", "6",]
-let array = ["Images/img1.webp", "Images/img2.webp", "Images/flower1.jpeg", "Images/flower2.jpg", "Images/image.avif", "Images/image3.jpeg"];
+let text = ["1", "2", "3", "4", "5", "6", "7"]
+let array = ["Images/Taj-Mahal-India.jpg", "Images/Christ-the-Redeemer-Brazil.jpg", "Images/Colosseum-Rome.jpg", "Images/EI-Castillo-Maxico.png", "Images/Machu-Picchu-Peru.jpeg", "Images/petra-Jordan.jpg", "Images/Wall-of-China.png"];
 // console.log(array[2])
 
 next.addEventListener("click", () => {
     // console.log("preeti")
     i++;
-    if (i >= array.length-1) i = 0;
+    if (i >= array.length - 1) i = 0;
     images.setAttribute("src", array[i]);
     images.setAttribute("alt", text[i]);
     console.log(images)
+
+    function randomColor() {
+        let colors = ["yellow", "pink", "aqua", "lightgreen", "orange","wheat"]
+        // for(leti=0; i<str.length; i++)
+        return colors[Math.floor(Math.random() * colors.length)];
+
+    }
+
+    next.addEventListener("click", () => {
+        document.body.style.backgroundColor = randomColor();
+    })
 })
 
 back.addEventListener("click", () => {
     // console.log("Kumari")
     i--;
-    if (i < 0) i = array.length-1;
+    if (i < 0) i = array.length - 1;
     images.setAttribute("src", array[i]);
     images.setAttribute("alt", text[i]);
     console.log(images)
 
+    function randomColor() {
+        let colors = ["yellow", "pink", "aqua", "lightgreen", "orange"]
+        // for(leti=0; i<str.length; i++)
+        return colors[Math.floor(Math.random() * colors.length)];
+
+    }
+
+    back.addEventListener("click", () => {
+        document.body.style.backgroundColor = randomColor();
+    })
+
 })
 
-function randomColor(){
-    let colors = ["yellow","pink","aqua","lightgreen","orange"]
-    // for(leti=0; i<str.length; i++)
-        return colors[Math.floor(Math.random()*colors.length)];
-    
-}
-
-document.addEventListener("click",()=>{
-    document.body.style.backgroundColor = randomColor();
-})
