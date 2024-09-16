69406259d612ce67d86e01a2ecba0445924cfe73
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2024-09-16 10:05:04 +0530

    some changes in js

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ let text = ["1", "2", "3", "4", "5", "6", "7"]
 let array = ["Images/Taj-Mahal-India.jpg", "Images/Christ-the-Redeemer-Brazil.jpg", "Images/Colosseum-Rome.jpg", "Images/EI-Castillo-Maxico.png", "Images/Machu-Picchu-Peru.jpeg", "Images/petra-Jordan.jpg", "Images/Wall-of-China.png"];
 // console.log(array[2])
 
+function randomColor() {
+    let colors = ["yellow", "pink", "aqua", "lightgreen", "orange","wheat"]
+    return colors[Math.floor(Math.random() * colors.length)];
+
+}
+
 next.addEventListener("click", () => {
     // console.log("preeti")
     i++;
@@ -14,17 +20,8 @@ next.addEventListener("click", () => {
     images.setAttribute("src", array[i]);
     images.setAttribute("alt", text[i]);
     console.log(images)
-
-    function randomColor() {
-        let colors = ["yellow", "pink", "aqua", "lightgreen", "orange","wheat"]
-        // for(leti=0; i<str.length; i++)
-        return colors[Math.floor(Math.random() * colors.length)];
-
-    }
-
-    next.addEventListener("click", () => {
-        document.body.style.backgroundColor = randomColor();
-    })
+    document.body.style.backgroundColor = randomColor();
+    
 })
 
 back.addEventListener("click", () => {
@@ -34,17 +31,8 @@ back.addEventListener("click", () => {
     images.setAttribute("src", array[i]);
     images.setAttribute("alt", text[i]);
     console.log(images)
-
-    function randomColor() {
-        let colors = ["yellow", "pink", "aqua", "lightgreen", "orange"]
-        // for(leti=0; i<str.length; i++)
-        return colors[Math.floor(Math.random() * colors.length)];
-
-    }
-
-    back.addEventListener("click", () => {
-        document.body.style.backgroundColor = randomColor();
-    })
+    document.body.style.backgroundColor = randomColor();
+    
 
 })
 
